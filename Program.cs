cbc599c1479cc2c8ec754b288bbbd997e3a9d302
Jakub <116199424+AsonoJake@users.noreply.github.com>
2023-04-13 12:36:30 +0200

    ??

--- Program.cs
+++ Program.cs
@@ -6,36 +6,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        // konstruktor BitMatrix(int, int, params int[])
-        // macierz 2x2, komplet danych w tablicy
-        var m = new BitMatrix(2, 2, new int[] { 1, 0, 0, 1 });
-        Console.WriteLine(m);
+        // // konstruktor BitMatrix(int, int, params int[])
+        // // macierz 2x2, komplet danych w tablicy
+        // var m = new BitMatrix(2, 2, new int[] { 1, 0, 0, 1 });
+        // Console.WriteLine(m);
 
-        m = new BitMatrix(2, 2, 1, 0, 0, 0);
-        Console.WriteLine(m);
-
-
-        // konstruktor BitMatrix(int, int, params int[])
-        // macierz 2x2, za dużo danych w tablicy
-        var n = new BitMatrix(2, 2, 1, 0, 0, 1, 1, 1, 0);
-        Console.WriteLine(n);
-
-        // macierz 3x2, za mało danych w tablicy
-        n = new BitMatrix(3, 2, 1, 0, 0, 1, 1);
-        Console.WriteLine(n);
+        // m = new BitMatrix(2, 2, 1, 0, 0, 0);
+        // Console.WriteLine(m);
 
 
-        // konstruktor BitMatrix(int[,])
-        int[,] arr = new int[,] { { 1, 0, 1 }, { 0, 1, 1 } };
-        var o = new BitMatrix(arr);
-        Console.WriteLine(arr.GetLength(0) == o.NumberOfRows);
-        Console.WriteLine(arr.GetLength(1) == o.NumberOfColumns);
-        Console.Write(o.ToString());
-        Console.WriteLine();
+        // // konstruktor BitMatrix(int, int, params int[])
+        // // macierz 2x2, za dużo danych w tablicy
+        // var n = new BitMatrix(2, 2, 1, 0, 0, 1, 1, 1, 0);
+        // Console.WriteLine(n);
 
-        var m1 = new BitMatrix(5, 6);
-        var m2 = m1;
-        Console.WriteLine(m1.Equals(m2));
+        // // macierz 3x2, za mało danych w tablicy
+        // n = new BitMatrix(3, 2, 1, 0, 0, 1, 1);
+        // Console.WriteLine(n);
+
+
+        // // konstruktor BitMatrix(int[,])
+        // int[,] arr = new int[,] { { 1, 0, 1 }, { 0, 1, 1 } };
+        // var o = new BitMatrix(arr);
+        // Console.WriteLine(arr.GetLength(0) == o.NumberOfRows);
+        // Console.WriteLine(arr.GetLength(1) == o.NumberOfColumns);
+        // Console.Write(o.ToString());
+        // Console.WriteLine();
+
+        // var m1 = new BitMatrix(5, 6);
+        // var m2 = m1;
+        // Console.WriteLine(m1.Equals(m2));
 
         var m3 = new BitMatrix(5, 6);
         var m4 = new BitMatrix(6, 5);
